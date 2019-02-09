a662bb1f726f80fc8936bc5a3cd869fc9d9b6bcd
Joseph Spurrier <github@josephspurrier.com>
2019-02-09 12:58:13 -0500

    Fix the filepath in the test file

--- testdata/example1/main.go
+++ testdata/example1/main.go
@@ -1,4 +1,4 @@
-//go:generate goversioninfo -icon=icon.ico -manifest=goversioninfo.exe.manifest
+//go:generate goversioninfo -64 -icon=../resource/icon.ico -manifest=../resource/goversioninfo.exe.manifest
 
 package main
 
